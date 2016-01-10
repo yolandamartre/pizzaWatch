78d4ceb96c9f6923d43ccc02486fd5f4c931882c
yolandamartre <yolandamartre@hotmail.com>
2016-01-10 12:33:01 -0600

    Initial Commit

---
+++ pizzaParaWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  pizzaParaWatch WatchKit Extension
+//
+//  Created by Yolanda Martínez on 1/10/16.
+//  Copyright © 2016 Yolanda Martínez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ pizzaParaWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pizzaParaWatch
+//
+//  Created by Yolanda Martínez on 1/10/16.
+//  Copyright © 2016 Yolanda Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
